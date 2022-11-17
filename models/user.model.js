bc298b703cb75a5939ea5b69e190b1c3cb0a5a2b
Ksenia Dorofeeva <kseniadorofeeva@Ksenias-MacBook-Pro.local>
2022-11-17 18:41:20 +0100

    final version

---
+++ models/user.model.js
@@ -0,0 +1,53 @@
+const bcrypt = require('bcryptjs');
+const mongodb = require('mongodb');
+
+const db = require('../data/database');
+
+class User {
+  constructor(email, password, fullname, street, postal, city) {
+    this.email = email;
+    this.password = password;
+    this.fullname = fullname;
+    this.address = {
+      street: street,
+      postal: postal,
+      city: city
+    };
+  }
+
+  static findById(userId) {
+    const uid = new mongodb.ObjectId(userId);
+
+    return db.getDb().collection('users').findOne({_id: uid }, {projection: {password: 0} });
+  }
+
+  getUserWithSameEmail() {
+    return db.getDb().collection('users').findOne({ email: this.email });
+  }
+
+  async existsAlready() {
+    const existingUser = await this.getUserWithSameEmail();
+    if (existingUser) {
+      return true;
+    }
+    return false;
+  }
+
+  async signup() {
+    const hashedPassword = await bcrypt.hash(this.password, 12);
+
+    await db.getDb().collection('users').insertOne({
+      email: this.email,
+      password: hashedPassword,
+      fullname: this.fullname,
+      address: this.address
+    });
+
+  }
+
+  hasMatchingPassword(hashedPassword) {
+    return bcrypt.compare(this.password, hashedPassword);
+  }
+}
+
+module.exports = User;
